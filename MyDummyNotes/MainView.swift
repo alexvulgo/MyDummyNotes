182a941c18be0f477beff5ed65281726e844e3e3
alexvulgo <120048791+alexvulgo@users.noreply.github.com>
2023-11-14 12:35:16 +0100

    Updated .gitignore
    Adding DS_STORE

---
+++ MyDummyNotes/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  MyDummyNotes
+//
+//  Created by Alessandro Esposito Vulgo Gigante on 14/11/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Starting NC1!")
+    }
+}
+
+#Preview {
+    MainView()
+}
