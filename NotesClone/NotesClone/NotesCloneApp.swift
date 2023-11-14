b22ee866d8ccd52a04eaa1ecb4b8e3854d996230
alexvulgo <120048791+alexvulgo@users.noreply.github.com>
2023-11-14 11:58:55 +0100

    Creation of Project
    First commit

---
+++ NotesClone/NotesClone/NotesCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesCloneApp.swift
+//  NotesClone
+//
+//  Created by Alessandro Esposito Vulgo Gigante on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SwiftUIView()
+        }
+    }
+}
---
+++ NotesClone/NotesClone/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  NotesClone
+//
+//  Created by Alessandro Esposito Vulgo Gigante on 14/11/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
